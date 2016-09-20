e1d5bafb576ce63dbe7aced3c7ce9b8cedbec2ce
telton <telton@umflint.edu>
2016-09-20 10:25:26 -0400

    Complete Element class unit test.

---
+++ tests/ElementTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use \UMFlint\Html\Element;
+
+class ElementTest extends \PHPUnit\Framework\TestCase
+{
+    public function testGetType()
+    {
+        $div = new Element('div');
+
+        $this->assertEquals('div', $div->getType());
+    }
+
+    public function testRender()
+    {
+        $div = new Element('div');
+        $this->assertEquals('<div></div>', $div->render());
+    }
+
+    public function testRenderChildren()
+    {
+        $div = new Element('div');
+        $div->appendChild('text');
+
+        $this->assertEquals('<div>text</div>', $div->render());
+    }
+
+    public function testIsVoid()
+    {
+        // Is void.
+        $br = new Element('br');
+        $this->assertEquals(1, $br->isVoid());
+
+        // Is not void.
+        $div = new Element('div');
+        $this->assertNotEquals(1, $div->isVoid());
+    }
+
+    public function testAddRemoveClass()
+    {
+        $div = new Element('div');
+
+        // Add.
+        $div->addClass('button');
+        preg_match('/class="(.*?)"/', $div->render(), $classes);
+        $this->assertEquals('button', $classes[1]);
+
+        // Remove.
+        $div->removeClass('button');
+        preg_match('/class="(.*?)"/', $div->render(), $classes);
+        $this->assertEquals('', $classes[1]);
+    }
+
+    public function testGetAppendPrependEmptyChildren()
+    {
+        $div = new Element('div');
+
+        // Append.
+        $div->appendChild('text');
+        $this->assertEquals('text', $div->getChildren()[0]);
+
+        // Prepend.
+        $div->prependChild('NEW');
+        $this->assertEquals('NEW', $div->getChildren()[0]);
+
+        // Empty.
+        $div->emptyChildren();
+        $this->assertEmpty($div->getChildren());
+    }
+}
