906c33e53c76c7192d3fc96d63033cc56428966e
Donald Wilcox <dowilcox@umflint.edu>
2016-09-21 09:12:04 -0400

    Get errors from session.

--- src/Html/HtmlServiceProvider.php
+++ src/Html/HtmlServiceProvider.php
@@ -4,6 +4,7 @@ namespace UMFlint\Html;
 
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
+use Illuminate\Session\SessionManager;
 use Illuminate\Support\ServiceProvider;
 use UMFlint\Html\Form\Form;
 
@@ -23,8 +24,15 @@ class HtmlServiceProvider extends ServiceProvider
         $this->app->singleton('umflint.html.form', function ($app) {
             $config = $app['config']['html'];
             $request = $app->make(Request::class);
+            $session = $app->make(SessionManager::class);
 
-            return new Form($config, $request->old());
+            $form = new Form($config);
+            $form->old($request->old());
+            if ($session->has('errors')) {
+                $form->setErrors($session->get('errors'));
+            }
+
+            return $form;
         });
 
         $this->app->alias('umflint.html.form', Form::class);
